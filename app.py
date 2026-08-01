8e07dc08c3d9aea7c6cbe3ac30fd124d2ea2dbc0
Vanessa-Harper <hap3rr0us3@gmail.com>
2026-08-01 14:11:03 -0400

    moved comma

--- app.py
+++ app.py
@@ -594,8 +594,8 @@ if st.session_state.payment_method:
         if "Gift Card" in st.session_state.payment_method:
             if gift_code:
                 st.success(
-                    f"🎉 Thank you for donating ${donation} using {st.session_state.payment_method}!"
-                    st.subheader("1. Balloons : celebrate a win"),
+                    f"🎉 Thank you for donating ${donation} using {st.session_state.payment_method}!",
+                    st.subheader("1. Balloons : celebrate a win")
 c1, c2 = st.columns(2)
 if c1.button("🎈 Balloons"):
     st.balloons()
@@ -605,8 +605,8 @@ if c1.button("🎈 Balloons"):
         else:
             if card_number and card_name and expiry and cvv:
                 st.success(
-                    f"🎉 Thank you for donating ${donation} using {st.session_state.payment_method}!"
-                    st.subheader("1. Balloons : celebrate a win"),
+                    f"🎉 Thank you for donating ${donation} using {st.session_state.payment_method}!",
+                    st.subheader("1. Balloons : celebrate a win")
 c1, c2 = st.columns(2)
 if c1.button("🎈 Balloons"):
     st.balloons()
